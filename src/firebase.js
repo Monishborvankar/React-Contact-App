817423cc3e14061d5456eadd66b08b26294824de
Monish Borvankar <monishborwankar@gmail.com>
2023-05-27 13:34:42 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+
+const firebaseConfig = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
+  };
+
+const fireDb = firebase.initializeApp(firebaseConfig);
+export default fireDb.database().ref();
